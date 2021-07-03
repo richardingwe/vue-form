cecc10fc06ff12f88bdec0ca9bfb381b0ac565c9
richardingwe <richardingwe5@gmail.com>
2021-07-03 15:58:48 +0100

    first commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,94 @@
+<template>
+	<form>
+		<label>Email:</label>
+		<input type="email" v-model="email" required />
+
+		<label>Password:</label>
+		<input type="password" v-model="password" required />
+
+		<label>Role:</label>
+		<select v-model="role">
+			<option value="developer">Web Developer</option>
+			<option value="designer">Web Designer</option>
+		</select>
+
+		<label>Skills (press alt + comma to add):</label>
+		<input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+		<div v-for="skill in skills" :key="skill" class="pill">
+			{{ skill }}
+		</div>
+
+		<div class="terms">
+			<input type="checkbox" v-model="terms" required />
+			<label>Accept terms and conditions</label>
+		</div>
+	</form>
+
+	<p>Email: {{ email }}</p>
+	<p>Password: {{ password }}</p>
+	<p>Your role: {{ role }}</p>
+	<p>Terms accepted: {{ terms }}</p>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				email: '',
+				password: '',
+				role: 'developer',
+				terms: false,
+				skills: [],
+				tempSkill: '',
+			};
+		},
+		methods: {
+			addSkill($event) {
+				console.log($event);
+				if ($event.key === ',' && this.tempSkill) {
+					if (!this.skills.includes(this.tempSkill)) {
+						this.skills.push(this.tempSkill);
+					}
+					this.tempSkill = '';
+				}
+			},
+		},
+	};
+</script>
+
+<style>
+	form {
+		max-width: 420px;
+		margin: 30px auto;
+		background: white;
+		text-align: left;
+		padding: 40px;
+		border-radius: 10px;
+	}
+	label {
+		color: #aaa;
+		display: inline-block;
+		margin: 25px 0 15px;
+		font-size: 0.6em;
+		text-transform: uppercase;
+		letter-spacing: 1px;
+		font-weight: bold;
+	}
+	input,
+	select {
+		display: block;
+		padding: 10px 6px;
+		width: 100%;
+		box-sizing: border-box;
+		border: none;
+		border-bottom: 1px solid #ddd;
+		color: #555;
+	}
+	input[type='checkbox'] {
+		display: inline-block;
+		width: 16px;
+		margin: 0 10px 0 0;
+		position: relative;
+		top: 2px;
+	}
+</style>
