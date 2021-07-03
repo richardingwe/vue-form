8fdd0cec7828ac4e5b99ca69fd16c54f92dc304a
richardingwe <richardingwe5@gmail.com>
2021-07-03 16:05:47 +0100

    done with vue forms

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,10 +1,11 @@
 <template>
-	<form>
+	<form @submit.prevent="handleSubmit">
 		<label>Email:</label>
 		<input type="email" v-model="email" required />
 
 		<label>Password:</label>
 		<input type="password" v-model="password" required />
+		<div v-if="passwordError" class="error">{{ passwordError }}</div>
 
 		<label>Role:</label>
 		<select v-model="role">
@@ -15,19 +16,18 @@
 		<label>Skills (press alt + comma to add):</label>
 		<input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
 		<div v-for="skill in skills" :key="skill" class="pill">
-			{{ skill }}
+			<span @click="deleteSkill(skill)">{{ skill }}</span>
 		</div>
 
 		<div class="terms">
 			<input type="checkbox" v-model="terms" required />
 			<label>Accept terms and conditions</label>
 		</div>
-	</form>
 
-	<p>Email: {{ email }}</p>
-	<p>Password: {{ password }}</p>
-	<p>Your role: {{ role }}</p>
-	<p>Terms accepted: {{ terms }}</p>
+		<div class="submit">
+			<button>Create an Account</button>
+		</div>
+	</form>
 </template>
 
 <script>
@@ -40,16 +40,36 @@
 				terms: false,
 				skills: [],
 				tempSkill: '',
+				passwordError: null,
 			};
 		},
 		methods: {
 			addSkill($event) {
-				console.log($event);
 				if ($event.key === ',' && this.tempSkill) {
 					if (!this.skills.includes(this.tempSkill)) {
 						this.skills.push(this.tempSkill);
 					}
 					this.tempSkill = '';
+				}
+			},
+			deleteSkill(skill) {
+				this.skills = this.skills.filter((item) => {
+					return skill !== item;
+				});
+			},
+			handleSubmit() {
+				// validate password
+				this.passwordError =
+					this.password.length > 5
+						? ''
+						: 'Password must be at least 6 characters long';
+				if (!this.passwordError) {
+					// make request to database to save user
+					console.log('email: ', this.email);
+					console.log('password: ', this.password);
+					console.log('role: ', this.role);
+					console.log('skills: ', this.skills);
+					console.log('terms accepted: ', this.terms);
 				}
 			},
 		},
@@ -90,5 +110,34 @@
 		margin: 0 10px 0 0;
 		position: relative;
 		top: 2px;
+	}
+	.pill {
+		display: inline-block;
+		margin: 20px 10px 0 0;
+		padding: 6px 12px;
+		background: #eee;
+		border-radius: 20px;
+		font-size: 12px;
+		letter-spacing: 1px;
+		font-weight: bold;
+		color: #777;
+		cursor: pointer;
+	}
+	button {
+		background: #0b6dff;
+		border: 0;
+		padding: 10px 20px;
+		margin-top: 20px;
+		color: white;
+		border-radius: 20px;
+	}
+	.submit {
+		text-align: center;
+	}
+	.error {
+		color: #ff0062;
+		margin-top: 10px;
+		font-size: 0.8em;
+		font-weight: bold;
 	}
 </style>
